794ab659486cfd74d1ce9883183ee57dbd80a076
Christopher Wheatley <chris@christopherwheatley.uk>
2018-10-25 14:22:10 +0100

    Add plugin for sass support

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     description: 'Christopher Wheatley is a software developer with 3 years commercial experience in the United Kingdom.',
   },
   plugins: [
+    'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
   ],
 };
