5d09cca2d7906654942c37fc66005ef8c2db37f7
Christopher Wheatley <chris@christopherwheatley.uk>
2018-11-10 16:39:31 +0000

    Outline the curriculum vitae page

--- src/pages/curriculum-vitae.jsx
+++ src/pages/curriculum-vitae.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import PageMetadata from '../components/PageMetadata';
 
 export default () => (
   <Layout>
-    <h1>Curriculum Vitae</h1>
+    <PageMetadata
+      title="Curriculum Vitae"
+      description="Find out about Christopher Wheatley's skills, experience and qualifications in software development."
+    />
+    <main>
+      <article>
+        <section>
+          <h1>Curriculum Vitae</h1>
+          <p>
+            Developer with a degree in computer science and 4 years commercial experience using
+            web technologies. Conscientious, comitted and friendly.
+          </p>
+          <section>
+            <h2>Skills</h2>
+          </section>
+          <section>
+            <h2>Employment</h2>
+          </section>
+          <section>
+            <h2>Education</h2>
+          </section>
+          <section>
+            <h2>Interests</h2>
+          </section>
+        </section>
+      </article>
+    </main>
   </Layout>
 );
