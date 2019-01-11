5089f50afd667d23db4c4d4d0ca57d49fa68eb12
Christopher Wheatley <chris@christopherwheatley.uk>
2019-01-11 17:10:56 +0000

    Make the curriculum-vitae page the index page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,8 +19,7 @@ function Header() {
           <div className="Header__container constrained">
             <div className="Header__title" to="/">{data.site.siteMetadata.title}</div>
             <nav>
-              <Link className="Header__link" to="/about">About Me</Link>
-              <Link className="Header__link" to="/curriculum-vitae">Curriculum Vitae</Link>
+              <Link className="Header__link" to="/">About</Link>
               <Link className="Header__link" to="/contact">Contact</Link>
             </nav>
           </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import PageMetadata from '../components/PageMetadata';
+import SkillList from '../components/SkillList';
+import EmploymentHistory from '../components/EmploymentHistory';
+import EducationHistory from '../components/EducationHistory';
+import InterestList from '../components/InterestList';
+import '../styles/panel.scss';
 
 export default () => (
   <Layout>
-    <h1>About</h1>
+    <PageMetadata
+      title="Curriculum Vitae"
+      description="Find out about Christopher Wheatley's skills, experience and qualifications in software development."
+    />
+    <article>
+      <div className="panel panel--pageHeader">
+        <h1 className="panel__header">Curriculum Vitae</h1>
+        <p className="panel__body">
+          Developer with a degree in computer science and 4 years commercial experience using web
+          technologies. Conscientious, comitted and friendly.
+        </p>
+      </div>
+      <section className="panel">
+        <h2 className="panel__header">Skills</h2>
+        <div className="panel__body">
+          <SkillList />
+        </div>
+      </section>
+      <section className="panel">
+        <h2 className="panel__header">Employment</h2>
+        <div className="panel__body">
+          <EmploymentHistory />
+        </div>
+      </section>
+      <section className="panel">
+        <h2 className="panel__header">Education</h2>
+        <div className="panel__body">
+          <EducationHistory />
+        </div>
+      </section>
+      <section className="panel">
+        <h2 className="panel__header">Interests</h2>
+        <div className="panel__body">
+          <InterestList />
+        </div>
+      </section>
+    </article>
   </Layout>
 );
