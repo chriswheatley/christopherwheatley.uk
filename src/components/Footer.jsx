c6f01451294205b82fd1c7a14426134e38de918a
Christopher Wheatley <chris@christopherwheatley.uk>
2019-03-04 14:49:19 +0000

    Footer tweaks Make the footer a bit more subtle and add name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,20 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer">
-      <div className="constrained">
-        Built with&nbsp;
-        <a href="https://www.gatsbyjs.org/">GatsbyJS</a>
+      <div className="footer__content constrained">
+        <div>
+          <div>Built by Christopher Wheatley</div>
+          <div>
+            <a
+              className="footer__link"
+              href="https://www.gatsbyjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Made with GatsbyJS
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
