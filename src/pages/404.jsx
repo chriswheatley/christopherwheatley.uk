310f83f392f631ced14894f627e6b19d39f47a8c
Christopher Wheatley <chris@christopherwheatley.uk>
2019-03-04 02:23:29 +0000

    Add content to 404 page Add an explanation of the error and possible reasons it might have occurred.

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { FaArrowRight } from 'react-icons/fa';
 import Layout from '../components/Layout';
+import PageMetadata from '../components/PageMetadata';
 
 export default () => (
   <Layout>
-    <h1>Not Found</h1>
+    <PageMetadata title="Not Found" />
+    <div className="panel panel--pageHeader">
+      <h1 className="panel__header">
+        404 - Not Found
+      </h1>
+    </div>
+    <div className="panel">
+      <div className="panel__body">
+        Sorry, the page you were looking for does not exist. This might be because:
+        <ul>
+          <li>There might be a typo in the url or address bar.</li>
+          <li>The page has been moved.</li>
+          <li>The page has been deleted.</li>
+          <li>The page has been removed temporarily.</li>
+        </ul>
+        <Link to="/" className="button">
+          <FaArrowRight className="button__icon" />
+          Return to site home
+        </Link>
+      </div>
+    </div>
   </Layout>
 );
